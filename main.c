b1d649de924284ab301c229f0cace3f1e8949aa3
Jambato <kagekyio@gmail.com>
2013-10-08 22:02:14 +0200

    Handle SIGINT and SDL_Init error

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
 #include <SDL2/SDL.h>
+
+void sig_handler(int signo)
+{
+    if (signo == SIGINT) {
+        exit(0);
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    signal(SIGINT, sig_handler);
+
+    if (SDL_Init(SDL_INIT_VIDEO) == -1) {
+        fprintf(stderr, "SDL_Init failure : %s\n :(\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+    }
 
     SDL_Quit();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
