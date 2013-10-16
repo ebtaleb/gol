9340181175459a56f81e47dcdd29ce7a680d4d1b
Jambato <kagekyio@gmail.com>
2013-10-16 21:32:58 +0200

    Add initialization and teardown functions

--- main.c
+++ main.c
@@ -13,6 +13,38 @@ void sig_handler(int signo)
     }
 }
 
+void init(SDL_Window **w, SDL_Surface **s)
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "SDL_Init failure : %s\n :(\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    //Create window
+    *w = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( *w == NULL ) {
+        fprintf(stderr, "SDL_CreateWindow failure : %s\n :(\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    //Get window surface
+    *s = SDL_GetWindowSurface(*w);
+}
+
+void teardown(SDL_Window *w, SDL_Surface *s)
+{
+	//Deallocate surface
+	SDL_FreeSurface(s);
+	s = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(w);
+	w = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
 int main(int argc, char *argv[])
 {
     SDL_Window *window = NULL;
@@ -21,22 +53,10 @@ int main(int argc, char *argv[])
 
     signal(SIGINT, sig_handler);
 
-    if (SDL_Init(SDL_INIT_VIDEO) == -1) {
-        fprintf(stderr, "SDL_Init failure : %s\n :(\n", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-
-    window = SDL_CreateWindow("Hello gof!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-
-    if (window == NULL) {
-        fprintf(stderr, "SDL_CreateWindow failure : %s\n :(\n", SDL_GetError());
-        exit(EXIT_FAILURE);
-    } else {
-        surface = SDL_GetWindowSurface(window);
-        SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
-        SDL_UpdateWindowSurface(window);
-        SDL_Delay(2000);
-    }
+    init(&window, &surface);
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(2000);
 
     while (1) {
         SDL_WaitEvent(&event);
@@ -46,9 +66,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
+    teardown(window, surface);
 
     return EXIT_SUCCESS;
 }
