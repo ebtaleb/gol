64ec40e7f8e7a43cfefed1d77508e5d294b1145a
Jambato <kagekyio@gmail.com>
2013-11-17 16:10:40 +0100

    Grid initialization now has its own function

--- gol.c
+++ gol.c
@@ -6,6 +6,11 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+typedef struct {
+    int x;
+    int y;
+} Point;
+
 void sig_handler(int signo)
 {
     if (signo == SIGINT) {
@@ -33,6 +38,46 @@ void init_display(SDL_Window **w, SDL_Renderer **r)
     }
 }
 
+void init_grid(Point *numcases, SDL_Renderer *renderer, SDL_Rect ****rect_array)
+{
+    int i = 0;
+    int j = 0;
+    int x = 0;
+    int y = 0;
+
+    *rect_array = (SDL_Rect ***)malloc(sizeof(SDL_Rect **)*numcases->y);
+
+    for (i = 0; i < numcases->x; i++) {
+        (*rect_array)[i] = (SDL_Rect **)malloc(sizeof(SDL_Rect *)*numcases->x);
+    }
+
+    for (i = 0; i < numcases->y; i++) {
+        for (j = 0; j < numcases->x; j++) {
+            (*rect_array)[i][j] = (SDL_Rect *)malloc(sizeof(SDL_Rect));
+            (*rect_array)[i][j]->x = x;
+            (*rect_array)[i][j]->y = y;
+            (*rect_array)[i][j]->h = 20;
+            (*rect_array)[i][j]->w = 20;
+            x += 21;
+        }
+        y += 21;
+        x = 0;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0x00);
+
+    for (i = 0; i < numcases->y; i++) {
+        for (j = 0; j < numcases->x; j++) {
+            SDL_RenderFillRect(renderer, (*rect_array)[i][j]);
+        }
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
 void teardown(SDL_Window *w, SDL_Renderer *r)
 {
     SDL_DestroyRenderer(r);
@@ -49,14 +94,12 @@ int main(int argc, char *argv[])
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
     SDL_Event event;
+    Point numcases = {SCREEN_WIDTH / 21, SCREEN_HEIGHT / 21};
 
     int numcases_x = SCREEN_WIDTH / 21;
     int numcases_y = SCREEN_HEIGHT / 21;
-    SDL_Rect *rect_array[numcases_y][numcases_x];
+    SDL_Rect ***rect_array = NULL;
     int i = 0;
-    int j = 0;
-    int x = 0;
-    int y = 0;
 
     int **gol_array = (int **)malloc(sizeof(numcases_y));
     for (i = 0; i < numcases_x; i++) {
@@ -66,35 +109,10 @@ int main(int argc, char *argv[])
 
     Uint8 color[4] = {0, 0, 0, 0};
 
-    for (i = 0; i < numcases_y; i++) {
-        for (j = 0; j < numcases_x; j++) {
-            rect_array[i][j] = (SDL_Rect *)malloc(sizeof(SDL_Rect));
-            rect_array[i][j]->x = x;
-            rect_array[i][j]->y = y;
-            rect_array[i][j]->h = 20;
-            rect_array[i][j]->w = 20;
-            x += 21;
-        }
-        y += 21;
-        x = 0;
-    }
-
     signal(SIGINT, sig_handler);
 
     init_display(&window, &renderer);
-
-    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-    SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0x00);
-
-    for (i = 0; i < numcases_y; i++) {
-        for (j = 0; j < numcases_x; j++) {
-            SDL_RenderFillRect(renderer, rect_array[i][j]);
-        }
-    }
-
-    SDL_RenderPresent(renderer);
+    init_grid(&numcases, renderer, &rect_array);
 
     while (1) {
         SDL_WaitEvent(&event);
